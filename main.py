71dcb2b95f0f8d472e329cb75e7df8d20557111d
Janno Potestades <70560144+jgpotestades@users.noreply.github.com>
2021-02-21 15:59:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,6 @@ def WelcomePage():
         sleep(1)
         WelcomePage()
 
-
 #===================================================
 #===============options from the menu===============
 def AddRecord():
